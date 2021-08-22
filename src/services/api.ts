adb0c47e269303cdd66308fad62377b97fb1f813
Luiz Henrique <7henrique18@gmail.com>
2021-08-22 14:33:14 -0300

    fix: api request

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,14 +4,16 @@ import md5 from "js-md5"
 const PUBLIC_KEY = process.env.NEXT_PUBLIC_MARVEL_API_PUBLIC_KEY
 const PRIVATE_KEY = process.env.NEXT_PUBLIC_MARVEL_API_PRIVATE_KEY
 
+const ts = +new Date()
+
 export const api = axios.create({
   baseURL: "http://gateway.marvel.com/v1/public/",
   params: {
-    apikey: process.env.NEXT_PUBLIC_MARVEL_API_PUBLIC_KEY,
-    ts: +new Date(),
+    apikey: PUBLIC_KEY,
+    ts: ts,
     hash: md5
       .create()
-      .update(+new Date() + (PRIVATE_KEY ? PRIVATE_KEY : "") + PUBLIC_KEY)
+      .update(ts + (PRIVATE_KEY ? PRIVATE_KEY : "") + PUBLIC_KEY)
       .toString()
   }
 })
